28ead351910e7a92eaf6ab0bdbc14908a2ae2b45
Oscar Sánchez <oscar.exe@gmail.com>
2014-11-15 16:31:46 -0600

    como eliminar, imagen funcioner, objetivos en el readme

---
+++ app/como eliminar.php
@@ -0,0 +1,20 @@
+<?php
+/*
+
+Como eliminar album.
+
+ejemplo: eliminar_album.php?albumid=3 
+$albumid = $_GET['albumid'];
+
+$db->del('albumes', array('albumid' => $albumid));
+
+
+
+*/
+
+
+
+
+
+
+
